4ec84ad7d4de7d06180523cfe86a9748b3fc8129
Awesomizer <christian.working@gmail.com>
2012-05-06 20:12:55 -0700

    Initial commit of raw Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LMCWeb::Application.config.secret_token = '9d73b1a2631d6cfd9b1c74bda49517ed6f9281811bd06b9cc4017758f2beded042eedaf4762f9f58311d89e1df657ce544f8814b8c397231a3a2532f9cd6c489'
